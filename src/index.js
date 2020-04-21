32a5a1895d8f7d95df19719fa9a02fed8af714f5
Eirik Skjærseth <eirik1502@gmail.com>
2020-04-21 23:24:05 +0200

    add endpoint for population queue

--- src/index.js
+++ src/index.js
@@ -13,24 +13,26 @@ app.use(bodyParser.json());
 const SIMULATION_DATA_QUEUE_LABEL = "queue:simulation-data";
 const SIMULATION_RESULT_QUEUE_LABEL = "queue:simulation-result";
 
+const POPULATION_QUEUE_LABEL = "queue:population";
+
 const redisClient = redis.createClient();
 redisClient.on("error", (err) => console.log(err));
 
+const getAllFromQueue = (label) =>  new Promise((resolve, reject) =>
+  redisClient.lrange(label, 0, -1, (err, queue) => {
+    resolve(queue.map(JSON.parse))
+  })
+);
+
 const getAllSimulations = () => {
-  return new Promise((resolve, reject) =>
-    redisClient.lrange(SIMULATION_DATA_QUEUE_LABEL, 0, -1, (err, queue) => {
-        resolve(queue.map(JSON.parse))
-      })
-  );
+  return getAllFromQueue(SIMULATION_DATA_QUEUE_LABEL)
 };
 
 const getAllSimulationsResults = () => {
-  return new Promise((resolve, reject) =>
-    redisClient.lrange(SIMULATION_RESULT_QUEUE_LABEL, 0, -1, (err, queue) => {
-      resolve(queue.map(JSON.parse))
-    })
-  );
+  return getAllFromQueue(SIMULATION_RESULT_QUEUE_LABEL)
 };
+
+const getAllPopulations = () => getAllFromQueue(POPULATION_QUEUE_LABEL)
 
 app.get("/api/simulationQueueConnected", (req, res) =>
   res.json(redisClient.connected)
@@ -61,5 +63,12 @@ app.post("/api/pushSimulation", (req, res) => {
     getAllSimulations().then((queue) => res.json(queue))
   );
 });
+
+
+app.get("/api/populations", (req, res) =>
+  getAllPopulations().then((queue) => {
+    res.json(queue)
+  })
+);
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
